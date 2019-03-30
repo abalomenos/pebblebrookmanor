9892eed60458f3df654280d83bf74b71a8248da8
Argiris Balomenos <arisbalomenos@gmail.com>
2019-03-30 12:48:11 -0700

    dotenv

---
+++ config/config.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql'
+  },
+
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+    logging: false
+  },
+  production: {
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql'
+  }
+}
