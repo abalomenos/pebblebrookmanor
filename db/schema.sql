eef78080b2e9247fc24eac2aade789d136e7b1b6
Jacksona750 <jacksona750@gmail.com>
2019-04-01 18:41:43 -0700

    added employee routes, not fully working

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,11 @@ USE eventdb;
 
 CREATE TABLE employees (
     id INT AUTO_INCREMENT NOT NULL,
-	employeeName VARCHAR(255) NOT NULL,
-	employeeWage INT NOT NULL,
-	employeeImage VARCHAR(255) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	wage INT NOT NULL,
+	image VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id)
 );
+
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
