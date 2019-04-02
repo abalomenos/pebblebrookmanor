29ad58bc6dba0a05644e40a80abf4c1fb2d80e80
Argiris Balomenos <arisbalomenos@gmail.com>
2019-04-01 19:21:45 -0700

    Update index

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,7 @@
 USE eventdb;
-INSERT INTO employees(name, wage, image) VALUES ("Sam", "23", "https://randomuser.me/api/portraits/men/25.jpg"), ("Julie", "22", "https://randomuser.me/api/portraits/women/18.jpg"), ("Sally", "23", "https://randomuser.me/api/portraits/women/25.jpg"), ("Ben", "22", "https://randomuser.me/api/portraits/men/2.jpg");
+
+INSERT INTO employee (employee_name, employee_wage, employee_image)
+VALUES   ('Sam', '23', 'https://randomuser.me/api/portraits/men/25.jpg'),
+         ('Julie', '22', 'https://randomuser.me/api/portraits/women/18.jpg'),
+         ('Sally', '23', 'https://randomuser.me/api/portraits/women/25.jpg'),
+         ('Ben', '22', 'https://randomuser.me/api/portraits/men/2.jpg');
