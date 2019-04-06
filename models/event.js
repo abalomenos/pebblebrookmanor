fd98b4ca8348c46d2c7fc03a57776d62969f2ff3
Jacksona750 <jacksona750@gmail.com>
2019-04-06 12:52:33 -0700

    adding routes for saving tables to DB

--- models/event.js
+++ models/event.js
@@ -5,7 +5,17 @@ module.exports = function(sequelize, DataTypes) {
       eventDate: DataTypes.STRING,
       customerName: DataTypes.STRING,
       customerEmail: DataTypes.STRING,
-      tables: DataTypes.STRING,
+      tables: { 
+        type: DataTypes.STRING, 
+        get: function() {
+            return JSON.parse(this.getDataValue('tables'));
+        }, 
+        set: function(val) {
+          console.log(val)
+          console.log(JSON.stringify(val))
+            return this.setDataValue('tables', JSON.stringify(val));
+        }
+      },
       employees: DataTypes.STRING,
       roomName: DataTypes.STRING,
       partySize: DataTypes.INTEGER
