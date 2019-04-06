664e154efa4c9ee5dfacae2e1f9a8bb4b51732a6
abalomenos <arisbalomenos@gmail.com>
2019-04-06 01:03:09 -0700

    Back-End changes, htmlRoutes.js done

--- public/js/main.js
+++ public/js/main.js
@@ -40,25 +40,25 @@ function dragMoveListener(event) {
 
 $(function() {
   /* SET PARAMETERS */
-  var change_img_time = 5000;
-  var transition_speed = 100;
+  var changeImgTime = 5000;
+  var transitionSpeed = 100;
 
-  var simple_slideshow = $("#exampleSlider"),
-    listItems = simple_slideshow.children("li"),
+  var simpleSlideshow = $("#exampleSlider"),
+    listItems = simpleSlideshow.children("li"),
     listLen = listItems.length,
     i = 0,
     changeList = function() {
-      listItems.eq(i).fadeOut(transition_speed, function() {
+      listItems.eq(i).fadeOut(transitionSpeed, function() {
         i += 1;
         if (i === listLen) {
           i = 0;
         }
-        listItems.eq(i).fadeIn(transition_speed);
+        listItems.eq(i).fadeIn(transitionSpeed);
       });
     };
 
   listItems.not(":first").hide();
-  setInterval(changeList, change_img_time);
+  setInterval(changeList, changeImgTime);
 });
 
 // ************** Slide Show End **************
@@ -124,9 +124,6 @@ $("#saveEvent").click(function() {
 
     // Need to save to DataBase
     console.log(tablesArray);
-    API.updateEvent().then(function(data) {
-
-    })
   });
 });
 
@@ -136,6 +133,7 @@ $("#saveEvent").click(function() {
 // ************* Get Event by Date **************
 $("#searchEvent").click(function() {
   var date = moment(new Date($("#datepicker").val())).format("MM-DD-YYYY");
+  console.log(date);
   $.ajax({
     url: "api/events/" + date,
     type: "GET"
@@ -220,134 +218,6 @@ $(function() {
 });
 
 
-// ************** Routing ********************
 
-// Get references to page elements
-var $submitEvent = $("#submit-event");
-var $eventList = $("#event-list");
-var $eventDate = $("#datepicker");
-var $customerName = $("#customerName");
-var $customerEmail = $("#customerEmail");
-var $roomName = $("#roomName");
-var $partySize = $("#partySize");
-var $searchEvent = $("#searchEvent");
-
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveEvent: function(event) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/events",
-      data: JSON.stringify(event)
-    });
-  },
-  getEvents: function() {
-    return $.ajax({
-      url: "api/events",
-      type: "GET"
-    });
-  },
-  deleteEvent: function(id) {
-    return $.ajax({
-      url: "api/events/" + id,
-      type: "DELETE"
-    });
-  },
-  updateEvent: function() {
-    return $.ajax({
-      url: "api/events/",
-      type: "PUT"
-    });
-  }
-};
-
-// Gets new event from the db and repopulates the list
-var refreshEvents = function() {
-  API.getEvents().then(function(data) {
-    var $events = data.map(function(event) {
-      var $a = $("<a>")
-        .text(event.customerName)
-        .attr("href", "/event/" + event.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": event.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("x");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $eventList.empty();
-    $eventList.append($events);
-  });
-};
-
-// addEvent is called whenever a new event is created
-// Save the new event to the db and refresh the list
-var addEvent = function(event) {
-  event.preventDefault();
-
-  var event = {
-    eventDate: $eventDate
-      .val()
-      .trim()
-      .split("/")
-      .join("-"),
-    customerName: $customerName.val().trim(),
-    customerEmail: $customerEmail.val().trim(),
-    roomName: $roomName.val().trim(),
-    partySize: $partySize.val().trim()
-  };
-
-  if (
-    !(
-      event.eventDate &&
-      event.customerName &&
-      event.customerEmail &&
-      event.roomName &&
-      event.partySize
-    )
-  ) {
-    alert("Please make sure to fill in all the form fields");
-    return;
-  }
-
-  console.log(event);
-
-  API.saveEvent(event).then(function() {
-    refreshEvents();
-  });
-
-  $eventDate.val("");
-  $customerName.val("");
-  $customerEmail.val("");
-  $roomName.val("");
-  $partySize.val("");
-};
-
-var deleteEvent = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteEvent(idToDelete).then(function() {
-    refreshEvents();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitEvent.on("click", addEvent);
-$eventList.on("click", ".delete", deleteEvent);
 
 // ************** Routing ********************
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,76 +3,79 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.render("index", {});
+    res.render("index");
   });
 
   // Grande Room Page
   app.get("/grande", function(req, res) {
-    res.render("grande", {});
+    res.render("grande");
   });
 
   // Venti Room Page
   app.get("/venti", function(req, res) {
-    res.render("venti", {});
+    res.render("venti");
   });
 
   // Luxor Room Page
   app.get("/luxor", function(req, res) {
-    res.render("luxor", {});
+    res.render("luxor");
   });
 
   // Reservation Page
   app.get("/reservation", function(req, res) {
-    db.Event.findAll({}).then(function(dbEvents) {
-      res.render("reservation", {
-        events: dbEvents
-      });
-    });  
+    res.render("reservation");
   });
 
-  // // admin Page
-  // app.get("/admin", function(req, res) {
-  //   db.Event.findAll({}).then(function(dbEvents) {
-  //     res.render("admin", {
-  //       events: dbEvents
-  //     });
-  //   });  
-  // });
-
-  // Temp for now
-  app.get("/temp", function(req, res) {
+  // All Events Page
+  app.get("/events", function(req, res) {
     db.Event.findAll({}).then(function(dbEvents) {
-      db.Employee.findAll({}).then(function(dbEmployees) {
-        res.render("temp", {
-          events: dbEvents,
-          employees: dbEmployees
-        });
+      res.render("allevents", {
+        // Need Plural
+        events: dbEvents
       });
     });
   });
 
-  // Load Events for admin
-  app.get("/admin", function(req, res) {
-      res.render("admin", {});
-  });
-
   // Load Events page and pass in an Event by id
-  app.get("/event/:id", function(req, res) {
+  app.get("/events/:id", function(req, res) {
     db.Event.findOne({ where: { id: req.params.id } }).then(function(dbEvent) {
       res.render("event", {
+        // Need single
         event: dbEvent
       });
     });
   });
 
-  // Load Employee page and pass in an Employee by id
-  app.get("/employee/:id", function(req, res) {
-    // eslint-disable-next-line prettier/prettier
-    db.Employee.findOne({ where: { id: req.params.id } }).then(function(dbEmployee) {
-      res.render("employee", {
-        employee: dbEmployee
+  // All Employees Page
+  app.get("/employees", function(req, res) {
+    db.Employee.findAll({}).then(function(dbEmployees) {
+      res.render("allemployees", {
+        // Need Plural
+        employees: dbEmployees
       });
     });
+  });
+
+  // Load Employee page and pass in an Employee by id
+  app.get("/employees/:id", function(req, res) {
+    db.Employee.findOne({ where: { id: req.params.id } }).then(function(
+      dbEmployees
+    ) {
+      res.render("employee", {
+        // Need single
+        employee: dbEmployees
+      });
+    });
+  });
+
+  // Add Employee Page
+  app.get("/addemployee", function(req, res) {
+      res.render("addemployee")
+  });
+
+  // Load Admin Page
+  app.get("/admin", function(req, res) {
+    res.render("admin");
   });
 
   // Render 404 page for any unmatched routes
