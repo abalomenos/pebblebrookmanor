b0ffe4db0773bd90be408ca2411c4fb4d8df4c4c
abalomenos <arisbalomenos@gmail.com>
2019-04-07 16:33:06 -0700

    admin update

--- models/event.js
+++ models/event.js
@@ -8,12 +8,23 @@ module.exports = function(sequelize, DataTypes) {
       tables: { 
         type: DataTypes.STRING, 
         get: function() {
+          try{
             return JSON.parse(this.getDataValue('tables'));
+          }
+          catch(err) {
+            console.log(err)
+          }
+            
         }, 
         set: function(val) {
           console.log(val)
           console.log(JSON.stringify(val))
+          try {
             return this.setDataValue('tables', JSON.stringify(val));
+          }
+          catch(err) {
+            console.log(err)
+          }
         }
       },
       employees: DataTypes.STRING,
